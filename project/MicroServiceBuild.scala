465c8bb16024b4abefc3909e6e592ea720c113ca
Luke Smith <luke.smith@digital.hmrc.gov.uk>
2017-06-26 12:10:52 +0100

    Updated naming and fixed a few copy errors

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -15,7 +15,7 @@ object MicroServiceBuild extends Build with MicroService {
     "uk.gov.hmrc" %% "play-reactivemongo" % "5.2.0",
     "org.reactivemongo" %% "play2-reactivemongo" % "0.12.0",
     ws,
-    "uk.gov.hmrc" %% "microservice-bootstrap" % "5.15.0",
+    "uk.gov.hmrc" %% "microservice-bootstrap" % "5.16.0",
     "uk.gov.hmrc" %% "play-authorisation" % "4.3.0",
     "uk.gov.hmrc" %% "play-health" % "2.1.0",
     "uk.gov.hmrc" %% "play-url-binders" % "2.1.0",
