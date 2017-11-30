343525efa7cd6edc2b9a3e79250ccc7731a74f08
paul.tan <paul.tan@digital.hmrc.gov.uk>
2017-11-30 14:43:22 +0000

    [APB-1691][AO, PT]: Added CesaRelationshipCopyService and Tests.

---
+++ app/uk/gov/hmrc/agentfirelationship/services/CesaRelationshipCopyService.scala
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.agentfirelationship.services
+
+import javax.inject.{Inject, Singleton}
+
+import play.api.Logger
+import play.api.mvc.Request
+import uk.gov.hmrc.agentfirelationship.audit.{AuditData, AuditService}
+import uk.gov.hmrc.agentfirelationship.connectors.{DesConnector, MappingConnector}
+import uk.gov.hmrc.agentmtdidentifiers.model.Arn
+import uk.gov.hmrc.domain.{Nino, SaAgentReference}
+import uk.gov.hmrc.http.HeaderCarrier
+
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class CesaRelationshipCopyService @Inject()(des: DesConnector,
+                                            mapping: MappingConnector,
+                                            auditService: AuditService) {
+
+  def lookupCesaForOldRelationship(arn: Arn, nino: Nino)
+                                  (implicit ec: ExecutionContext, hc: HeaderCarrier, request: Request[Any], auditData: AuditData): Future[Set[SaAgentReference]] = {
+    auditData.set("nino", nino)
+    for {
+      references <- des.getClientSaAgentSaReferences(nino)
+      matching <- intersection(references) {
+        mapping.getSaAgentReferencesFor(arn)
+      }
+      _ = auditData.set("saAgentRef", matching.mkString(","))
+      _ = auditData.set("CESARelationship", matching.nonEmpty)
+    } yield {
+      matching
+    }
+  }
+
+  private def intersection[A](cesaIds: Seq[A])(mappingServiceCall: => Future[Seq[A]])(implicit ec: ExecutionContext, hc: HeaderCarrier): Future[Set[A]] = {
+    val cesaIdSet = cesaIds.toSet
+
+    if (cesaIdSet.isEmpty) {
+      Logger.warn("The sa references in cesa are empty.")
+      Future.successful(Set.empty)
+    } else
+      mappingServiceCall.map { mappingServiceIds =>
+        val intersected = mappingServiceIds.toSet.intersect(cesaIdSet)
+        Logger.info(s"The sa references in mapping store are $mappingServiceIds. The intersected value between mapping store and DES is $intersected")
+        intersected
+      }
+  }
+
+}
