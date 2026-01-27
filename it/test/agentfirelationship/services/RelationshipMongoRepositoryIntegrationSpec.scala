2a92a6764676fba4aff755c41e38e4be2d8e27ec
Mathew May <29102292+mghmay@users.noreply.github.com>
2026-01-27 17:36:00 +0000

    APB-10444-2 Fix Ispec

--- it/test/agentfirelationship/services/RelationshipMongoRepositoryIntegrationSpec.scala
+++ it/test/agentfirelationship/services/RelationshipMongoRepositoryIntegrationSpec.scala
@@ -18,13 +18,16 @@ package agentfirelationship.services
 
 import java.time.LocalDateTime
 import javax.inject.Singleton
+
+import scala.concurrent.duration.DurationInt
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+
 import agentfirelationship.agentId
 import agentfirelationship.clientId
 import agentfirelationship.service
 import agentfirelationship.support.UpstreamServicesStubs
-import org.mongodb.scala.model.Filters.{equal => mongoEqual}
+import org.mongodb.scala.model.Filters.{ equal => mongoEqual }
 import org.mongodb.scala.model.Filters.and
 import org.scalatestplus.play.guice.GuiceOneServerPerSuite
 import play.api.inject.guice.GuiceApplicationBuilder
@@ -38,8 +41,6 @@ import uk.gov.hmrc.agentfirelationship.models.RelationshipStatus.Terminated
 import uk.gov.hmrc.agentfirelationship.repository.RelationshipMongoRepository
 import uk.gov.hmrc.agentfirelationship.support.UnitSpec
 import uk.gov.hmrc.mongo.test.CleanMongoCollectionSupport
-
-import scala.concurrent.duration.DurationInt
 
 @Singleton
 class RelationshipMongoRepositoryIntegrationSpec
@@ -292,7 +293,7 @@ class RelationshipMongoRepositoryIntegrationSpec
             .toFuture()
         )(60.seconds)
 
-        await(repo.collection.countDocuments().toFuture()) shouldBe 1000001
+        await(repo.collection.countDocuments().toFuture()) shouldBe 200001
 
         await(
           repo.removeNinoSuffixBulk()
