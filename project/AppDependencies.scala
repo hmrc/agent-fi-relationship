137b5950dccc17448636ea1bf74c49e401e91520
geoffreywatson <9043337+geoffreywatson@users.noreply.github.com>
2026-09-18 13:13:51 +0100

    [APB-12180][GW] update library dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,21 +4,23 @@ import play.sbt.PlayImport.ws
 
 object AppDependencies {
 
-  private val bootstrapVer: String = "10.7.0"
-  private val mongoVer: String     = "2.13.0"
-  private val playVer: String    = "play-30"
+  private val bootstrapVer: String  = "10.8.0"
+  private val mongoVer: String      = "2.14.0"
+  private val playVer: String       = "play-30"
+  private val domainVer: String     = "13.0.0"
 
   lazy val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc"       %% s"bootstrap-backend-$playVer" % bootstrapVer,
     "uk.gov.hmrc.mongo" %% s"hmrc-mongo-$playVer"        % mongoVer,
-    "uk.gov.hmrc"       %% s"domain-$playVer"            % "11.0.0"
+    "uk.gov.hmrc"       %% s"domain-$playVer"            % domainVer
   )
 
   lazy val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% s"bootstrap-test-$playVer"  % bootstrapVer,
     "uk.gov.hmrc.mongo" %% s"hmrc-mongo-test-$playVer" % mongoVer,
-    "org.scalacheck"    %% "scalacheck"                % "1.19.0"
+    "org.scalacheck"    %% "scalacheck"                % "1.19.0",
+    "uk.gov.hmrc"       %% s"domain-test-$playVer"     % domainVer
   ).map(_ % Test)
 
 }
